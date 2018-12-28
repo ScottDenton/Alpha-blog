7e305d8f90a292d8fcd9222effac722daf12fa7d
ScottDenton <scottyd_14@hotmail.com>
2018-12-28 09:55:57 -0800

    Added authentication and restrictions on UI

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ resources :users, except: [:new]
 
 resources :articles
 
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
